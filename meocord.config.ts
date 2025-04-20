1fd84dde0168140fccd47054d26264d5340fab07
L A Romeo <ukasyahrz@outlook.com>
2025-04-21 01:06:48 +0700

    build(webpack): add configuration for asset handling
    Add webpack configuration to process markdown, HTML, images, and fonts. Define output paths and exclude node_modules to ensure proper asset organization.

--- meocord.config.ts
+++ meocord.config.ts
@@ -2,5 +2,21 @@ import { MeoCordConfig } from 'meocord/interface'
 
 export default {
   appName: 'MeoCord',
-  discordToken: '<your-token-here>'
+  discordToken: '<your-token-here>',
+  webpack: config => {
+    config.module.rules?.push({
+      test: /\.(md|html)$/i,
+      type: 'asset/source',
+    })
+    config.module.rules?.push({
+      test: /\.(gif|jpg|jpeg|png|svg|woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      exclude: /node_modules/,
+      generator: {
+        filename: 'assets/[name][ext]',
+      }
+    })
+
+    return config
+  },
 } satisfies MeoCordConfig
