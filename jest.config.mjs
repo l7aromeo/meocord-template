c45a3e6d5938bba1573d6e9eabdc96ed900734dc
L A Romeo <ukasyahrz@outlook.com>
2026-04-12 01:57:06 +0700

    fix: update jest config for ESM path resolution and inversify transforms

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,10 +2,18 @@
 export default {
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
-    '^@src/(.+)\\.js$': '<rootDir>/src/$1.ts',
+    '^@src/(.+)\\.js$': '<rootDir>/src/$1',
+    '^@src/(.+)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
-    '^.+\\.ts$': ['@swc/jest', { jsc: { parser: { syntax: 'typescript', decorators: true }, transform: { decoratorMetadata: true } } }],
+    '^.+\\.ts$': ['@swc/jest', { jsc: { parser: { syntax: 'typescript', decorators: true }, transform: { decoratorMetadata: true }, target: 'es2022' }, module: { type: 'es6' } }],
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(inversify|@inversifyjs/common|@inversifyjs/container|@inversifyjs/core|@inversifyjs/plugin|@inversifyjs/prototype-utils|@inversifyjs/reflect-metadata-utils))',
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   testMatch: ['**/*.spec.ts'],
+  clearMocks: true,
+  restoreMocks: true,
 }
